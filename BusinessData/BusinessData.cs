461f42e63e9f7cceb3aab4d2d10e021f2f71c861
Umer <sheikhumer973@gmail.com>
2020-10-24 03:09:51 +0530

    Startup of LeadPortalAPI

---
+++ BusinessData/BusinessData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessData
+{
+    public class BusinessData : IBusinessData
+    {
+        public Object objResult;
+
+        public Int64 CreateUser()
+        {
+            return 1;
+        }
+
+        public object GetUsers()
+        {
+            objResult = null;
+            objResult = "Users";
+            return objResult;
+        }
+    }
+}
---
+++ LeadPortalAPI/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿
+namespace LeadPortalAPI.Controllers
+{
+    using System.Web.Http;
+    using BusinessData;
+    public class UserController : ApiController
+    {
+        IBusinessData ibusiness;
+
+        public UserController()
+        {
+            ibusiness = new BusinessData();
+        }
+
+        public IHttpActionResult GET()
+        {
+            return Ok("API is running");
+        }
+
+        [Route("api/GetUsers")]
+        public IHttpActionResult GetUsers()
+        {
+            return Ok(ibusiness.GetUsers());
+        }
+
+        [Route("api/CreateUser")]
+        public IHttpActionResult POST()
+        {
+            return Ok(ibusiness.CreateUser());
+        }
+    }
+}
